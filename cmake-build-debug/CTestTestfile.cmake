5cd294e9e7c187036968b6cd68355e963145d63a
lapsizm <78648610+lapsizm@users.noreply.github.com>
2021-11-13 22:27:47 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kirill/lapsizm/repet
-# Build directory: /home/kirill/lapsizm/repet/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/kirill/lapsizm/repet/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/kirill/lapsizm/repet/CMakeLists.txt;60;add_test;/home/kirill/lapsizm/repet/CMakeLists.txt;0;")
